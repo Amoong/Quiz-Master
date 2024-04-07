5f36549cba7145b692d67db20a3eb2b84773faab
Amoong <moo1323@naver.com>
2024-04-07 19:52:23 +0900

    Add answer check logic

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -1,7 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using Microsoft.Unity.VisualStudio.Editor;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using Image = UnityEngine.UI.Image;
 
 
 public class Quiz : MonoBehaviour
@@ -9,11 +12,52 @@ public class Quiz : MonoBehaviour
     [SerializeField] TextMeshProUGUI questionText;
     [SerializeField] QuestionSO question;
     [SerializeField] GameObject[] answerButtons;
+    [SerializeField] Sprite defaultAnswerSprite;
+    [SerializeField] Sprite correctAnswerSprite;
 
     void Start()
     {
-        questionText.text = question.GetQuestion();
+        // DisplayQuestion();
+        GetNextQuestion();
+    }
 
+    public void OnAnswerSelected(int index)
+    {
+        if (index == question.GetCorrectAnswerIndex())
+        {
+            questionText.text = "Correct!";
+            Image buttonImage = answerButtons[index].GetComponent<Image>();
+            buttonImage.sprite = correctAnswerSprite;
+        }
+        else
+        {
+            int correctAnswerIndex = question.GetCorrectAnswerIndex();
+
+            questionText.text = "Sorry, the correct answer was\n" + question.GetAnswer(correctAnswerIndex);
+            answerButtons[correctAnswerIndex].GetComponent<Image>().sprite = correctAnswerSprite;
+        }
+
+        SetButtonState(false);
+    }
+
+    void GetNextQuestion()
+    {
+        SetButtonState(true);
+        SetDefaultButtonSprites();
+        DisplayQuestion();
+    }
+
+    void SetDefaultButtonSprites()
+    {
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
+            answerButtons[i].GetComponent<Image>().sprite = defaultAnswerSprite;
+        }
+    }
+
+    void DisplayQuestion()
+    {
+        questionText.text = question.GetQuestion();
 
         for (int i = 0; i < answerButtons.Length; i++)
         {
@@ -21,5 +65,13 @@ public class Quiz : MonoBehaviour
             buttonsText.text = question.GetAnswer(i);
         }
 
+    }
+
+    void SetButtonState(bool state)
+    {
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
+            answerButtons[i].GetComponent<Button>().interactable = state;
+        }
     }
 }

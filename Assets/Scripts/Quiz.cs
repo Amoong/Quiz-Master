2c1984214708d86a73006148de03ad88d55f99cc
Amoong <moo1323@naver.com>
2024-04-07 15:23:29 +0900

    Link answers to buttons

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -8,10 +8,18 @@ public class Quiz : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI questionText;
     [SerializeField] QuestionSO question;
+    [SerializeField] GameObject[] answerButtons;
 
     void Start()
     {
         questionText.text = question.GetQuestion();
-    }
 
+
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
+            TextMeshProUGUI buttonsText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
+            buttonsText.text = question.GetAnswer(i);
+        }
+
+    }
 }
